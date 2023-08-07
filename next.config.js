4c90a865089c0daf052d605941a190d10c830a20
Victor Monsalve <victrokwtf@gmail.com>
2023-08-07 00:10:33 -0500

    add configurations

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      unoptimized: true,
+      remotePatterns: []
+    },
+    swcMinify: true,
+    webpack: (config, options) => {
+      const { dir, defaultLoaders } = options;
+      config.resolve.extensions.push('.ts', '.tsx');
+      config.module.rules.push({
+        test: /\.tsx?$/,
+        include: [dir],
+        exclude: /node_modules/,
+        use: [
+          defaultLoaders.babel,
+          {
+            loader: 'ts-loader',
+            options: { compilerOptions: { noEmit: false } }
+          }
+        ]
+      });
+  
+      return config;
+    }
+  };
+  
+  module.exports = nextConfig;
